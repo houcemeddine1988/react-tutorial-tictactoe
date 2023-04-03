411e459830ede6bf659902936a574b5c09eca3d9
houcemeddine1988 <houcemeddinebenmanser@gmail.com>
2023-04-04 00:43:26 +0100

    set up

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/src/components/Layout"
+import Square from "@/src/components/app"
+
+
+
+export default function Home() {
+  return (
+    <Layout>
+      <Square/>
+    </Layout>      
+  )
+}
